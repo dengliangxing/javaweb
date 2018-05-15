164cbeedc0c317647d4b8912a803ef73e4614a89
dengliangxing <dengliangxing@gmail.com>
2018-05-15 14:47:33 +0800

    chapter 1 commit

---
+++ chapter01/src/com/itheima/ioc/TestIoc.java
@@ -0,0 +1,13 @@
+package com.itheima.ioc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestIoc {
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		UserDao userDao = (UserDao) applicationContext.getBean("userDao");
+		userDao.say();
+		
+	}
+}

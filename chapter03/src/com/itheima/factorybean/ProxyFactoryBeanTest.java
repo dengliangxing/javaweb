990ea49f2ba45ab17022bdf44e8288cb118bd0f9
dengliangxing <dengliangxing@gmail.com>
2018-05-16 09:23:49 +0800

    9:23 chapter3 3commit

---
+++ chapter03/src/com/itheima/factorybean/ProxyFactoryBeanTest.java
@@ -0,0 +1,16 @@
+package com.itheima.factorybean;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.itheima.jdk.UserDao;
+
+public class ProxyFactoryBeanTest {
+	public static void main(String[] args) {
+		String xmlpath = "com/itheima/factorybean/applicationContext.xml";
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(xmlpath);
+		UserDao userDao = (UserDao) applicationContext.getBean("userDaoProxy");
+		userDao.addUser();
+		userDao.deleteUser();
+	}
+}
